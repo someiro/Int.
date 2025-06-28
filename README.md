bf58e7020fc9526cfdf6933b441e544e6a2ed14c
そめいろ <someiro.2007@gmail.com>
2025-06-28 20:23:54 +0900

    "npm create vite@latest" と top pageの作成、それらに必要なコンポーネントの実装。

---
+++ README.md
@@ -0,0 +1,4 @@
+<div style="text-align: center">
+<h1>Int.</h1>
+<p>Minimum Design System</p>
+</div>
---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./app.css";
+import { format } from "@formkit/tempo"
+
+function App() {
+  const readable = format(new Date(), "YYYY")
+  return (
+    <>
+      <div className="intHeader">
+        <div className="intHeaderContainer">
+          <h1>Int.</h1>
+          <div className="intHeaderMenu">
+            <button className="intButton">About</button>
+            <button className="intButton">Components</button>
+            <button className="intOutlinedButton">GitHub</button>
+          </div>
+        </div>
+      </div>
+      <div className="intContainer intTextCenter">
+        <h1 className="intTextVeryLarge intAddMargin">Int.</h1>
+        <p className="intTextSmall intAddMargin">Minimum Design System</p>
+      </div>
+      <div className="intContainer intRemoveBackgroundColor intRemovePadding intTextVerySmall">
+        <p className="intTextCenter intAddPadding">Copyright (c) {readable} someiro. All Rights Reserved.</p>
+      </div>
+    </>
+  )
+}
+
+export default App;
